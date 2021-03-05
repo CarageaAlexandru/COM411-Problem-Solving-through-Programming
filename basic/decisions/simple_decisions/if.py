a9ade25fea413e0116fbc51fc0d6e019fdabcc85
repl.it user <replituser@example.com>
2021-03-05 10:38:31 +0000

    Added code to demonstrate the use of an if statement.

---
+++ basic/decisions/simple_decisions/if.py
@@ -0,0 +1,6 @@
+print("Hello there, please enter a book type, e.g: Adventure, Sci-fi, Romance")
+answer = input()
+
+if (answer == "adventure"):
+    print("I like adventure books!")
+print("Finished reading book!")
