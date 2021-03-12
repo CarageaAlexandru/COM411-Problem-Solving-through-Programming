80a71c502cd08111eec6ccdd7c1d78bac116ae7a
repl.it user <replituser@example.com>
2021-03-12 10:53:15 +0000

    functions with nesting

---
+++ basic/functions/function_with_nesting.py
@@ -0,0 +1,10 @@
+def identify():
+    print("What lies before us?")
+    answer = input()
+
+    if answer == "a large boulder":
+        print("it's time to run")
+    else: 
+        print("We will be fine")
+
+identify()
